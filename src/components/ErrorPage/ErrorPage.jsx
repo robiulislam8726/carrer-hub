664e4bee61624c8990a1969b233763d634669435
robiulislam8726 <robiul.islam8726@gmail.com>
2024-09-20 17:11:06 +0900

    added career hub code

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,10 @@
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h2>Oops!!</h2>
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import Root from './components/Root/Root';
+import Home from './components/Home/Home';
+import AppliedJobs from './components/AppliedJobs/AppliedJobs';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children :[
+      {
+        path : '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/applied',
+        element : <AppliedJobs></AppliedJobs>
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
