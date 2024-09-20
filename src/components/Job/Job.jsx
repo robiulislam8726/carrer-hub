ed1b02466fa7405ccf9697993c554b258d1a4225
robiulislam8726 <robiul.islam8726@gmail.com>
2024-09-20 20:48:43 +0900

    added data load section in the code

---
+++ src/components/Job/Job.jsx
@@ -0,0 +1,29 @@
+import { MdOutlineAddLocationAlt } from "react-icons/md";
+import { PiCurrencyCircleDollarBold } from "react-icons/pi";
+
+const Job = ({ job }) => {
+    const {logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
+    return (
+        <div className="card card-compact bg-base-100  shadow-xl">
+            <figure>
+                <img src={logo} alt="Shoes" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{job_title}</h2>
+                <p>{company_name}</p>
+                <div>
+                    <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">{remote_or_onsite}</button>
+                    <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">{job_type}</button>
+                </div>
+                <div className=" flex mt-4">
+                    <h2 className="flex mr-4"><MdOutlineAddLocationAlt className="text-2xl mr-2"></MdOutlineAddLocationAlt>{location}</h2>
+                    <h2 className="flex"><PiCurrencyCircleDollarBold className="text-2xl mr-2 "></PiCurrencyCircleDollarBold>{salary}</h2>
+                </div>
+                <div className="card-actions ">
+                    <button className="btn btn-primary">View Details</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Job;
